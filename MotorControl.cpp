0a02f26bad588521948cf0174ee3c5129ade0dae
Anthony Zeng <anthonyzeng99@gmail.com>
2017-05-15 10:54:55 -0400

    worked on MotorControl class

--- MotorControl.cpp
+++ MotorControl.cpp
@@ -1,4 +1,22 @@
-#include "Arduino.h"
 #include "MotorControl.h"
 
+
+MotorControl::MotorControl(int steps, int pin0, int pin1, int pin2, int pin3) {
+    Stepper motor = Stepper(steps, pin0, pin1, pin2, pin3);
+    _current_position = 0;
+}
+
+void MotorControl::set_speed(int step_speed) {
+  motor -> setSpeed(step_speed);
+}
+
+void MotorControl::step(int steps) {
+  motor -> step(steps);
+  _current_position += steps;
+}
+
+void MotorControl::home() {
+  motor -> step(_current_position * -1);
+  _current_position = 0;
+} 
 
--- MotorControl.h
+++ MotorControl.h
@@ -2,9 +2,22 @@
 #define MOTOR_CONTROL_H
 
 #include "Arduino.h"
+#include "Stepper.h"
 
-class MotorControl {
+class Motor {
+  
+};
 
+class MotorControl{
+  public:
+    MotorControl (int steps, int pin0, int pin1, int pin2, int pin3);
+    void set_speed(int speed);
+    void step(int steps);
+    void home();
+  
+  private:
+    int _current_position;
+    Stepper *motor;
 };
 
 #endif
